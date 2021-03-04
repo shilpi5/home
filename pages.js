ebf4f784a72e415c0c0bbee62543286462ab35e5
shilpi57 <shilpi57@gmail.com>
2021-03-04 12:13:23 +0530

    edited editable

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/shilpi57/home.git";
+const repoURL = "https://github.com/shilpi57/shilpi57.github.io";
 
 ghpages.publish(
   pathname,
